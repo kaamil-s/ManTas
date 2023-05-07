d788393fe1f4c6b1732468250e06a0783bcd21b3
kaamil-s <128225558+kaamil-s@users.noreply.github.com>
2023-05-07 14:36:58 +0530

    Add files via upload

---
+++ src/MyComponents/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            
+            <p>ManTas App is single page application made to help you manage your tasks.</p>
+            <p>It has a really easy to use and simple interface which allows you to make a list of all the important tasks you want to do.</p>
+            <p>It helps you to build a To-Do-List within no time.</p>
+        </div>
+       
+    )
+}
---
+++ src/MyComponents/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+export const Footer = () => { 
+    return (
+        <footer className="bg-dark text-light py-3">
+            <p className="text-center">
+            Copyright &copy; ManTasApp.com | Kaamil Savla
+            </p>
+        </footer>
+    )
+}
